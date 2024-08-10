9519fbed291042d15e2d585af7ebb4955773c77b
Andrew Paxson <13216842+paxsonsa@users.noreply.github.com>
2024-08-10 00:10:42 -0700

    Remove unused EngineSystem
    Signed-off-by: Andrew Paxson <13216842+paxsonsa@users.noreply.github.com>

--- crates/cinemotion-core/src/engine_systems.rs
+++
@@ -1,9 +0,0 @@
-use crate::prelude::*;
-use async_trait::async_trait;
-use bevy_ecs::prelude::*;
-
-#[async_trait]
-pub trait EngineSystem {
-    fn name(&self) -> String;
-    async fn update(&mut self, world: &mut World) -> Result<()>;
-}
